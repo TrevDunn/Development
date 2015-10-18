fe54c4f7eb4ae78f8c5c3ca940559068f613606f
Trevor Dunn <trevor.h.dunn4@gmail.com>
2015-10-18 18:44:56 -0400

    still trying to figure out the second part of the game; clicking on squares

--- js/app.js
+++ js/app.js
@@ -6,91 +6,60 @@ window.onload = function () {
 
 	//Start button to generate board. Will refactor later to decide how many players (comp vs 2p)
 	$('button#game-starter').click(function(){
-		drainOcean();
-		buildOcean();
+		GameModule.drainOcean();
+		GameModule.buildOcean();
+		GameModule.clearShips();
+		GameModule.generateShipA();
+		GameModule.generateShipB();
+		GameModule.generateShipC();
+		GameModule.generateShipD();
+		GameModule.generateShipF();
 	});
 
+		// Turns all Divs with 'empty' class into clickable, guessable buttons.
+
+
+	$('#place-aircraft-carrier').eq(0).click(function(){
+		console.log(this);
+
+	});
 };
 
-
-// Make board (not necessarily the guess or select board right now)
-var oceanBoardArray = [	['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
-						['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
-						['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
-						['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
-						['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
-						['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
-						['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
-						['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
-						['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
-						['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',]]
-
-
-
-// First attempt at div constructor loop for spaces; creates board inside game-board div
-var buildOcean = function(){
-
-	//creates the div game-board
-	var tempBoard = document.createElement('div');
-	tempBoard.className = 'game-board';
-	tempBoard.id = 'guess-board';
-	document.querySelector('div#container').appendChild(tempBoard);
-
-	//creates 'latitude' rows to contain individual square spaces
-	for (var i = 0; i < oceanBoardArray.length; i++) {
-		var tempRow = document.createElement('div');		
-		tempRow.classList.add('latitude');
-		tempRow.id = ('row' + i)  //Look up classList() use on MDN: Can we pass a variable (i) here? YES
-		tempBoard.appendChild(tempRow);
-
-		//Using nested for functions to create Square divs within each appended row
-		for (var j = 0; j < oceanBoardArray[i].length; j++) {  
-			var tempSquares = document.createElement('div');
-			tempSquares.classList.add('square-space','empty');
-			tempSquares.setAttribute('x-lat', i);
-			tempSquares.setAttribute('y-lon-', j);
-			tempSquares.innerHTML = oceanBoardArray[i][j];
-				//Use this random color board for win state - make function celebrationBoard on a loop
-			 tempSquares.setAttribute('style', 'background:rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) +',' + (Math.floor(Math.random() * 256)) + ');');
-			tempRow.appendChild(tempSquares);
-			console.log(oceanBoardArray[i][j]);
-		};
-	};
-};
-
-//Clear the grid if game is restarted
-var drainOcean = function() {
-		var tempBoard = document.querySelector('div.game-board');
-
-	if (tempBoard) {
-		var tempContainer = document.getElementById('container');
-		console.log(tempContainer, tempBoard);
-		tempContainer.removeChild(tempBoard);
-		
-	}
-};
+	$('div.empty').on("hover",(function() {
+		console.log(this);
+		GameModule.divGuess();
+	}));
 
 
 // Ship Placement logic - Have to do amost same thing for each type of ship? Can it be dynamic? Should it be?
 
 //Initialize placement of Aircraft Carrier
-$('#place-aircraft-carrier').eq(0).click(function(){
-	console.log(this);
-
-	//remove any currently place permenant AND temporary div classifications
-
-});
 
 
 
+
+// Computer Ship placement
+	// will utilize the module's hidden array so as to hide ship locations from Player1
+
+
+	
+
+var GameModule = function(){
 	var compCordsA = ['1','2','3','4','5'];		//Aircraft Carrier
 	var compCordsB = ['1','2','3','4'];			//Battleship
 	var compCordsC = ['1','2','3'];				//Cruiser
 	var compCordsD = ['1','2','3'];				//Destroyer
 	var compCordsF = ['1','2'];					//Frigate
-// Computer Ship placement
-	// will utilize the module's hidden array so as to hide ship locations from Player1
-
+	var oceanBoardArray = [	['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
+							['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
+							['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
+							['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
+							['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
+							['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
+							['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
+							['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
+							['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
+							['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',]]
 
 	var computerBoard = [	['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
 							['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
@@ -103,11 +72,50 @@ $('#place-aircraft-carrier').eq(0).click(function(){
 							['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',],
 							['&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp', '&nbsp',]]
 
-var computerPlacement = function(){
-
-	//check X axis spaces (to the right) for all 'empty' spaces
-
 	return {
+
+		// First attempt at div constructor loop for spaces; creates board inside game-board div
+		buildOcean : function(){
+
+			//creates the div game-board
+			var tempBoard = document.createElement('div');
+			tempBoard.className = 'game-board';
+			tempBoard.id = 'guess-board';
+			document.querySelector('div#container').appendChild(tempBoard);
+
+			//creates 'latitude' rows to contain individual square spaces
+			for (var i = 0; i < oceanBoardArray.length; i++) {
+				var tempRow = document.createElement('div');		
+				tempRow.classList.add('latitude');
+				tempRow.id = ('row' + i)  //Look up classList() use on MDN: Can we pass a variable (i) here? YES
+				tempBoard.appendChild(tempRow);
+
+				//Using nested for functions to create Square divs within each appended row
+				for (var j = 0; j < oceanBoardArray[i].length; j++) {  
+					var tempSquares = document.createElement('div');
+					tempSquares.classList.add('square-space','empty');
+					tempSquares.setAttribute('x-lat', i);
+					tempSquares.setAttribute('y-lon-', j);
+					tempSquares.innerHTML = oceanBoardArray[i][j];
+						//Use this random color board for win state - make function celebrationBoard on a loop
+					 //tempSquares.setAttribute('style', 'background:rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) +',' + (Math.floor(Math.random() * 256)) + ');');
+					tempRow.appendChild(tempSquares);
+					console.log(oceanBoardArray[i][j]);
+				};
+			};
+		},
+
+		//Clear the grid if game is restarted
+		drainOcean : function() {
+				var tempBoard = document.querySelector('div.game-board');
+
+			if (tempBoard) {
+				var tempContainer = document.getElementById('container');
+				console.log(tempContainer, tempBoard);
+				tempContainer.removeChild(tempBoard);
+				
+			}
+		},
 		//places Aircraft Carrier
 		generateShipA : function() {
 			var finalize = false;
@@ -330,42 +338,48 @@ var computerPlacement = function(){
 				};
 			};
 		},
+		clearShips : function() {
+			for (var i = 0; i < computerBoard.length; i++) {
+				for (var j = 0; j < computerBoard.length; j++) {
+					computerBoard[i][j] = '&nbsp';
+				};
+				
+			};
+		},
+		divGuess : function() {
+			console.log(this);
+			// if (this.) {
+
+			// } else{
+
+			// };
+		},
 	}; // end return
 }();
 
-computerPlacement.generateShipA();
-computerPlacement.generateShipB();
-computerPlacement.generateShipC();
-computerPlacement.generateShipD();
-computerPlacement.generateShipF();
 
-	// create a Counter that will end in lose state if == 0
-		//this will be temporary
+// var placeShip = function(xlat, ylon, xlat2, ylon2, shipArray) {
+// 	if (((xlat == xlat2) || (ylon == ylon2)) && (shipArray.length >= Math.abs(xlat - xlat2) && (shipArray.length >= Math.abs(ylon - ylon2)))) {
 
-
-var placeShip = function(xlat, ylon, xlat2, ylon2, shipArray) {
-	if (((xlat == xlat2) || (ylon == ylon2)) && (shipArray.length >= Math.abs(xlat - xlat2) && (shipArray.length >= Math.abs(ylon - ylon2)))) {
-
-	};
-}
+// 	};
+// }
 
 
 
 
-// HIDES ships on Guess board (player 2 board?)
-HideShips = (function(){
-	var aircraftCarrier = ['1','2','3','4','5'];
-	var battleship = ['1','2','3','4'];
-	var cruiser = ['1','2','3'];
-	var destroyer = ['1','2','3'];
-	var frigate = ['1','2'];
+// // HIDES ships on Guess board (player 2 board?)
+// HideShips = (function(){
+// 	var aircraftCarrier = ['1','2','3','4','5'];
+// 	var battleship = ['1','2','3','4'];
+// 	var cruiser = ['1','2','3'];
+// 	var destroyer = ['1','2','3'];
+// 	var frigate = ['1','2'];
 
-});
+// });
 
 
 // Make divs clickable - compare to ship locations
 	//On click, div checks ship location stats (stored in private module) to see if miss or hit, updates "empty" class on clicked div (use 'this?')
-
 
 
 
